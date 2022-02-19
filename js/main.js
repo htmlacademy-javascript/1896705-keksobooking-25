49c57955b1b0eb259794931467dfa4c460a3d443
maksvty <yurij1101@gmail.com>
2022-02-19 04:13:52 +0300

    Fix eslint

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function getRandomInteger (start, end) {
-  return Math.floor(Math.random() * (Math.abs(end - start) + 1)) + Math.min(start, end);
+  return Math.floor(Math.random() * (Math.abs(end - start) + 1)) + Math.min(start, end)
 };
 
 function getRandomFloat (start, end, floatSigns) {
-  return (Math.random() * Math.abs(end - start) + Math.min(start, end)).toFixed(floatSigns);
+  return (Math.random() * Math.abs(end - start) + Math.min(start, end)).toFixed(floatSigns)
 };
 
 getRandomInteger(1,10);
